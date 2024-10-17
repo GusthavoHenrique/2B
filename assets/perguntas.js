07904ee59d59d12e4ae2875c7cad57ef4fa97360
SilvaGusthavo <111454910+GusthavoHenrique@users.noreply.github.com>
2024-10-17 13:58:01 -0300

    commit

--- assets/perguntas.js
+++ assets/perguntas.js
@@ -13,3 +13,9 @@ criaCartao(
 criaCartao(
     'Programação', 'O que é uma função?', 'Uma função é um bloco de código que executa alguma tarefa'
 )
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em inglês se diz HI (RAI)'
+)
